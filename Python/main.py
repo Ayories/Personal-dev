8846543d85a7c882ecea1fd46e194ae99fa18ad9
Cre8ivexiv <147156522+Cre8ivexiv@users.noreply.github.com>
2026-02-23 13:20:50 +0000

    Create instances of InPatient and OutPatient
    Added instances of InPatient and OutPatient classes with sample data.

--- Python/main.py
+++ Python/main.py
@@ -10,4 +10,31 @@ def main():
     print("=== Patient ===")
     print(patient_fred)
     print()
+
+    inpatient = InPatient(
+        "Mary",
+        "9 Maple Avenue, Birmingham",
+        ["Asthma"],
+        doctor_john,
+        center,
+        "Ward 3",
+        "2026-02-22"
+    )
+
+    outpatient = OutPatient(
+        "Sam",
+        "44 Pine Close, Birmingham",
+        ["Back pain"],
+        doctor_john,
+        center,
+        "2026-02-25"
+    )
+
+    print("=== InPatient ===")
+    print InPatient
+    print()
+
+    print("=== OutPatient ===")
+    print OutPatient
+    print()
     
