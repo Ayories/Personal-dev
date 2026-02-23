42fddf3de7dde957cc175883b69123c49acaab83
Ayories <sanusiabdulquadri@gmail.com>
2026-02-23 11:57:43 +0000

    lab excercise

---
+++ Lab Applied prog/week 5/person.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 23 11:11:30 2026
+
+@author: sanus
+"""
+
+class person:
+    def __init__(self, name: str, address: str):
+        self.__name = name
+        self.__address = address
+    def get__name():
+        return self.__name
+    def get__address():
+        return self.__address
